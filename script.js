dd2cb3695a34088b700d397aa1a6706223d47f6a
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-10-05 12:48:12 -0500

    add event listeners for buttons

--- script.js
+++ script.js
@@ -22,4 +22,42 @@ function operator(calculationFunction, num1, num2) {
     return calculationFunction(num1, num2);
 }
 
-console.log(operator(subtract, 2, 6));
+// setting query selectors for all buttons
+let num0Button = document.querySelector(".num0");
+let num1Button = document.querySelector(".num1");
+let num2Button = document.querySelector(".num2");
+let num3Button = document.querySelector(".num3");
+let num4Button = document.querySelector(".num4");
+let num5Button = document.querySelector(".num5");
+let num6Button = document.querySelector(".num6");
+let num7Button = document.querySelector(".num7");
+let num8Button = document.querySelector(".num8");
+let num9Button = document.querySelector(".num9");
+let clearButton = document.querySelector(".clearButton")
+let deleteButton = document.querySelector(".deleteButton")
+let decimalButton = document.querySelector(".decimalButton")
+let equalsButton = document.querySelector(".equalsButton")
+let multiplyButton = document.querySelector(".multiplyButton")
+let divideButton = document.querySelector(".divideButton")
+let addButton = document.querySelector(".addButton")
+let subtractButton = document.querySelector(".subtractButton")
+
+// adding event listeners for all buttons
+num0Button.addEventListener("click", () => console.log("0"));
+num1Button.addEventListener("click", () => console.log("1"));
+num2Button.addEventListener("click", () => console.log("2"));
+num3Button.addEventListener("click", () => console.log("3"));
+num4Button.addEventListener("click", () => console.log("4"));
+num5Button.addEventListener("click", () => console.log("5"));
+num6Button.addEventListener("click", () => console.log("6"));
+num7Button.addEventListener("click", () => console.log("7"));
+num8Button.addEventListener("click", () => console.log("8"));
+num9Button.addEventListener("click", () => console.log("9"));
+clearButton.addEventListener("click", () => console.log("clear"));
+deleteButton.addEventListener("click", () => console.log("delete"));
+decimalButton.addEventListener("click", () => console.log("."));
+equalsButton.addEventListener("click", () => console.log("="));
+multiplyButton.addEventListener("click", () => console.log("X"));
+divideButton.addEventListener("click", () => console.log("//"));
+addButton.addEventListener("click", () => console.log("+"));
+subtractButton.addEventListener("click", () => console.log("-"));
